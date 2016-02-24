90665f68718dc707c25035d102fd3e8eb9d4d961
Maiju Schreck <K2322@student.jamk.fi>
2016-02-24 10:41:17 +0200

    <harjoituksia tunnilta>

---
+++ LayoutExampleApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LayoutExampleApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {   
+            //Piirretään UI XAML-koodista
+            this.InitializeComponent();
+            //Vaihdetaan ikkunan käynnistysmoodia
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //Ikkunan koko
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+
+            //Muokataan save buttonia. 
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Name:" + nameTextBox.Text);
+            Debug.WriteLine("Email:" + emailTextBox.Text);
+            // password ... ?
+            Debug.WriteLine("Password:" + passwordBox.Password);
+            if ((bool)userCheckBox.IsChecked) Debug.WriteLine("Normal User");
+            if ((bool)adminCheckBox.IsChecked) Debug.WriteLine("Admin");
+             
+        }
+    }
+}
